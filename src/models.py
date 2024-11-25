94b984b347c6d5e49336e1dff70ea40ac166b1bd
AnahiVera <vera.anahi.93@gmail.com>
2024-11-25 17:39:32 -0300

    users

--- src/models.py
+++ src/models.py
@@ -6,3 +6,26 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, unique=False, nullable=False)
+    is_active = db.Column(db.Boolean, default=True)
+
+    profile = db.relationship('Profile', backref="user")
+
+    def serialize(self):
+        return {
+            "id":self.id,
+            "username":self.username,
+            "is_active":self.is_active,
+            "profile": self.profile.serialize()
+        }
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
