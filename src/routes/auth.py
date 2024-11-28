f70fbbc677546a96c7d295fbb97c47921dfc906d
AnahiVera <vera.anahi.93@gmail.com>
2024-11-28 18:42:05 -0300

    models test

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -1,11 +1,23 @@
+import datetime
 from flask import Blueprint, request, jsonify
+from flask_jwt_extended import create_access_token
 from models import User, Profile
+from werkzeug.security import generate_password_hash, check_password_hash
 
 bp_auth = Blueprint("bp_auth", __name__)
 
 @bp_auth.route('/register', methods=['POST'])
 def register():
-    return jsonify({"path": "register"})
+    
+    username = request.json.get('username')
+    password = request.json.get('password')
+
+    if not username:
+        return jsonify({"message": "Username is required"}), 422
+    
+    if not password:
+        return jsonify({"message": "Password is required"}), 422
+
 
 @bp_auth.route('/login', methods=['POST'])
 def login():
