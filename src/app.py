03bd661ff4c14386d2ff2f55f3d9b73a3c10edad
AnahiVera <vera.anahi.93@gmail.com>
2024-12-30 14:09:05 -0300

    add user route

--- src/app.py
+++ src/app.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 from routes.auth import bp_auth 
 from routes.tasks import bp_tasks
 from routes.event import bp_event
+from models import User
 
 load_dotenv()
 
@@ -31,6 +32,35 @@ app.register_blueprint(bp_tasks, url_prefix="/api")
 @app.route('/')
 def main():
     return jsonify({"msg": "Server running correctly"}), 200
+
+@app.route('/api/users')
+def get_users():
+
+    users = User.query.all()
+    users= {user.name for user in users}
+
+    return jsonify({"users": users}), 200
+
+@app.route('/api/users', methods=['POST'])
+def add_user():
+
+    name = request.json.get('name')
+
+    if not name:
+        return jsonify({"status": "error", "message": "Name is required"}), 422
+    
+    found = User.query.filter_by(name=name).first()
+
+    if found: 
+        return jsonify({"status": "error", "message": "User already exists"}), 422
+    
+    user = User()
+    user.name = name
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({"status": "success", "message": "User added successfully"}), 201
 
 
 if __name__ == '__main__':
